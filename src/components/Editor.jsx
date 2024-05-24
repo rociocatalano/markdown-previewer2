bfcfcda1e6f3c08807bf666087ae4ebf0ee2ff91
Rocío Catalano <rociocatalano4@hotmail.com>
2024-05-24 16:00:53 -0300

    adding basic components

---
+++ src/components/Editor.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react';
+
+const Editor = () => {
+
+return <textarea name="editor" id="editor">Testing :)</textarea>
+}
+
+
+export default Editor;
---
+++ src/components/Preview.jsx
@@ -0,0 +1,7 @@
+import React, {useState} from 'react'; 
+
+const Preview = () => {
+    return <div id="preview">Previewing</div>
+}
+
+export default Preview;
